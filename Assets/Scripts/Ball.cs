88df7625144eec4ad6ad2e084a6349f8820c7938
Sharpaxe <sharpaxiee@gmail.com>
2020-07-16 15:00:42 -0700

    init

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour
+{
+    public int speed = 100;
+    public int Score;
+    
+    public static explicit operator Ball(GameObject v)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+
+        if (col.gameObject.name == "racket") {
+            // Calculate hit Factor
+            float x=hitFactor(transform.position,
+                              col.transform.position,
+                              col.collider.bounds.size.x);
+
+            // Calculate direction, set length to 1
+            Vector2 dir = new Vector2(x, 1).normalized;
+
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+        if (col.gameObject.tag == "Block")
+        {
+            AddScore();
+            speed++;
+        }
+        
+    }
+
+    float hitFactor(Vector2 ballPos, Vector2 racketPos,
+                float racketWidth) {
+
+        return (ballPos.x - racketPos.x) / racketWidth;
+    }
+
+    void AddScore()
+    {
+        Score++;
+        FindObjectOfType<Text>().text = "Score: " + Score * 100;
+    }
+
+    void Update()
+    {
+        if(GetComponent<Rigidbody2D>().position.y < -200)
+        {
+            FindObjectOfType<GameManager>().GameOver();
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameOver = false;
+    public float restartDelay = 3f;
+
+    public void GameOver()
+    {
+      
+        if (!gameOver)
+        {
+            gameOver = true;
+            Restart();
+            //gameOver = false;
+            Invoke("Restart", restartDelay);
+        }
+        
+    }
+
+    public void LevelCompleted()
+    {
+
+    }
+
+    void Restart()
+    {
+        //Reload the sceene that is currently loaded
+        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        //For debugging with infinite lives
+        //FindObjectOfType<Ball>().transform.position = new Vector2(0, 0);
+
+    }
+
+    private void Update()
+    {
+        if (GameObject.FindGameObjectsWithTag("Block").Length == 1)
+        {
+            Debug.Log("You Win!");
+            LevelCompleted();
+        }
+    }
+}
